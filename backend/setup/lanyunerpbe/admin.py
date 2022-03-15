0fce5da86279f93f428577f60dde800f44fd99f0
Chia-Hua Chang <CHChang810716@gmail.com>
2022-03-15 16:37:35 +0000

    update model

--- backend/setup/lanyunerpbe/admin.py
+++ backend/setup/lanyunerpbe/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Person, Property, ManageGroup, InstrGroup
 
 # Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Property)
+admin.site.register(ManageGroup)
+admin.site.register(InstrGroup)
--- backend/setup/lanyunerpbe/models.py
+++ backend/setup/lanyunerpbe/models.py
@@ -8,16 +8,33 @@ class Object(models.Model):
 class Person(Object):
   sn = models.CharField(max_length=128) # serial number
   sArYear = models.IntegerField(default = 1911) # school arrival year
+  name = models.CharField(max_length=64, default='xxx')
+  def __str__(self):
+    return '{}:{}:#{}'.format(self.sArYear, self.name, self.sn)
   
 class ManageGroup(Object):
   name = models.CharField(max_length=128)
+  def __str__(self):
+    return self.name
 
 class InstrGroup(Object):
   name = models.CharField(max_length=128)
+  def __str__(self):
+    return self.name
 
 class Property(Object):
   name = models.CharField(max_length=256)
   serialNum = models.CharField(max_length=2048)
   mgroup = models.ForeignKey(ManageGroup, on_delete=models.PROTECT)
   igroup = models.ForeignKey(InstrGroup, on_delete=models.PROTECT)
-  borrowedBy = models.ForeignKey(Person, null=True, on_delete=models.PROTECT)
+  borrowedBy = models.ForeignKey(Person, null=True, on_delete=models.PROTECT, blank=True)
+  def __str__(self):
+    pref = '#{}:{}:{}:{}'.format(
+      self.serialNum, 
+      self.name, 
+      self.mgroup,
+      self.igroup
+    )
+    if self.borrowedBy is not None:
+      return pref + ' borrowed by ' + self.borrowedBy.name
+    return pref
