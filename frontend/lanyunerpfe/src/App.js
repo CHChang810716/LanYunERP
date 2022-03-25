b85735e083cc225955cda97f8223aadb2cf4f281
Chia-Hua Chang <CHChang810716@gmail.com>
2022-03-26 00:10:25 +0800

    basic post works

--- frontend/lanyunerpfe/src/App.js
+++ frontend/lanyunerpfe/src/App.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import be from 'be';
-const LOADING = 0;
-const NO_AUTH = 1;
-const LOGINED = 2;
+import be from './be';
+const LOADING  = 0;
+const NO_AUTH  = 1;
+const LOGINED  = 2;
+const REGISTER = 3;
 
 function Header({loginState}) {
   return <div>{loginState}</div>
 }
 
+const e2value = (f) => { return (e) => f(e.target.value) }
+const useInputState = (...val) => {
+  const [v, setter] = useState(...val)
+  return [v, e2value(setter)]
+}
 const Loading = ({setLoginState}) => {
   async function getPersonInfo() {
     const person_info = await be.get('/person_info');
@@ -21,34 +27,76 @@ const Loading = ({setLoginState}) => {
   return <div>loading...</div>
 }
 const Login = ({setLoginState}) => {
-  const [username, setUsername] = useState('')
-  const [passwd, setPasswd] = useState('')
+  const [username, setUsername ] = useInputState('')
+  const [password, setPassword ] = useInputState('')
   async function postAuthData() {
-    const auth_res = await be.post('/login', {
-      'username': username,
-      'password': passwd
-    });
-    if(auth_res.code === 0) {
+    const form = new FormData()
+    form.append('username', username)
+    form.append('password', password)
+    const auth_res = await be.post('/login', form);
+    if(auth_res.data.code === 0) {
       setLoginState(LOGINED)
     } else {
       setLoginState(NO_AUTH)
     }
   }
-  const onSubmit = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
     postAuthData();
   }
   return <div>
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} method="post" action="login">
       <input type="text" value={username} onChange={setUsername} />
-      <input type="text" value={passwd} onChange={setPasswd} />
-      <input type="submit" value="Submit" />
+      <input type="password" value={password} onChange={setPassword} />
+      <input type="submit" value="登入" />
     </form>
+    <input type="button" onClick={() => setLoginState(REGISTER)} value="註冊"/>
   </div>
 }
 const User = ({setLoginState}) => {
   return <div></div>
 }
-
+const Register = ({setLoginState}) => {
+  const [username,   setUsername  ] = useInputState('')
+  const [password,   setPassword  ] = useInputState('')
+  const [email,      setEmail     ] = useInputState('')
+  const [first_name, setFirstName ] = useInputState('')
+  const [last_name,  setLastName  ] = useInputState('')
+  const [sn,         setSN        ] = useInputState('')
+  const [sArYear,    setsArYear   ] = useInputState('')
+  async function postData() {
+    const form = new FormData();
+    form.append("username",   username,  )
+    form.append("password",   password,  )
+    form.append("email",      email,     )
+    form.append("first_name", first_name,)
+    form.append("last_name",  last_name, )
+    form.append("sn",         sn,        )
+    form.append("sArYear",    sArYear,   )
+    const res = await be.post('/create_person', form);
+    if(res.data.code === 0) {
+      setLoginState(NO_AUTH)
+    } else {
+      // TODO: identify which field error
+    }
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    postData();
+  }
+  return <div>
+    <form onSubmit={onSubmit}>
+      <input type="text"      value={username}   onChange={setUsername}  />
+      <input type="password"  value={password}   onChange={setPassword}  />
+      <input type="text"      value={email}      onChange={setEmail}     />
+      <input type="text"      value={first_name} onChange={setFirstName} />
+      <input type="text"      value={last_name}  onChange={setLastName}  />
+      <input type="text"      value={sn}         onChange={setSN}        />
+      <input type="text"      value={sArYear}    onChange={setsArYear}   />
+      <input type="submit"    value="註冊" />
+    </form>
+  </div>
+}
 function App() {
   const [loginState, setLoginState] = useState(LOADING);
   const headerProps = {
@@ -61,12 +109,16 @@ function App() {
     setLoginState
   }
   const userProps = {}
+  const registerProps = {
+    setLoginState
+  }
   return (
     <div className="App">
       <Header {...headerProps} />
-      { loginState === LOADING && <Loading {...loadingProps}/>}
-      { loginState === NO_AUTH && <Login {...loginProps}/>}
-      { loginState === LOGINED && <User {...userProps}/>}
+      { loginState === LOADING  && <Loading {...loadingProps}/>}
+      { loginState === NO_AUTH  && <Login {...loginProps}/>}
+      { loginState === LOGINED  && <User {...userProps}/>}
+      { loginState === REGISTER && <Register {...registerProps}/>}
     </div>
   );
 }
--- frontend/lanyunerpfe/src/be.js
+++ frontend/lanyunerpfe/src/be.js
@@ -2,9 +2,18 @@ import _axios from "axios"
 
 const be = (baseURL) => {
   const instance = _axios.create({
-    baseURL: baseURL || 'http://132.126.126.66:8788/lanyunerpbe',
+    baseURL: baseURL || 'http://132.226.126.66:8788/lanyunerpbe',
     timeout: 1000,
   });
+  instance.interceptors.request.use(request => {
+    console.log('Starting Request', JSON.stringify(request, null, 2))
+    return request
+  })
+
+  instance.interceptors.response.use(response => {
+    console.log('Response:', JSON.stringify(response, null, 2))
+    return response
+  })
   return instance;
 }
 
